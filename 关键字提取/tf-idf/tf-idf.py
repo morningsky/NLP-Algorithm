c15efbbd981dd774135fe1985d9f9bc9542018da
morningsky <sky_lmc@foxmail.com>
2018-05-07 11:06:55 +0800

    textrank算法实现 停用字加入英文单词与英文符号

--- 关键字提取/tf-idf/tf-idf.py
+++ 关键字提取/tf-idf/tf-idf.py
@@ -11,14 +11,14 @@ import numpy as np
 import pandas as pd
 #读入停用词
 stopwords = []
-for line in open('chinese_stopword.txt','r',encoding='utf-8'):
+for line in open('chinese_english_stopword.txt','r',encoding='utf-8'):
     stopwords.append(line.rstrip()) #去掉尾部的空白字符
-    
+
 def genDoc(path, stopwords):
     '''
     生成文本集二重列表，每一个文件分词之后的词语作为一个列表
     所有文件生成的词汇表构成Doc列表
-    '''       
+    '''
     Doc = []
     word_list = set()
     title_list = list()
@@ -28,7 +28,7 @@ def genDoc(path, stopwords):
             with open(path + '/' + f, 'r', encoding='utf-8') as t:
                 #将空格、换行符去除
                 content = t.read().strip().replace('\n','')\
-                        .replace(' ','').replace('\r','').replace('\t','') 
+                        .replace(' ','').replace('\r','').replace('\t','')
                 #使用基于深度学习的FoolNltk分词
                 seg_content = list(jieba.cut(content))
                 seg_content_dropstop = []
@@ -38,16 +38,16 @@ def genDoc(path, stopwords):
                         word_list.add(word)
             Doc.append(seg_content_dropstop)
     return Doc,word_list,title_list
-    
+
 
 def tf(word, doc):
     return doc.count(word) / len(doc)
-    
+
 def idf(word, Doc):
     count = 0
     for each_doc in Doc:
         if word in each_doc:
-            count += 1        
+            count += 1
     return np.log(len(Doc)/(count+1))
 
 def getTFIDF(Doc, word_list, title_list):
@@ -66,11 +66,9 @@ def getTopk(df_tfidf, topk = 3):
     for doc_index, doc_words in df_tfidf.iterrows():
         print(doc_index)
         print(doc_words.sort_values(ascending=False)[0:topk])
-        
+
 if __name__ == "__main__":
     #测试
     Doc, word_list, title_list = genDoc(r'./data', stopwords)
     df_tfidf = getTFIDF(Doc, word_list, title_list)
     getTopk(df_tfidf, topk=5)
-
-    
